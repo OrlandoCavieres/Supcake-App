7fc82ce82329943861b78386eb69422595fa6198
Orlando Cavieres <frizortake@gmail.com>
2020-08-23 01:15:35 -0400

    Producto ahora hereda de interface Serializable, para poder acceder a nuevos productos y editar ya existentes.

--- app/src/main/java/com/example/myapplication/Producto.kt
+++ app/src/main/java/com/example/myapplication/Producto.kt
@@ -1,12 +1,18 @@
 package com.example.myapplication
 
+import java.io.Serializable
+
 class Producto(private val id: Int,
                var nombre: String,
                var precio: Int,
-               var cantidad: Int,
-               var imagen: Int) {
+               private var cantidad: Int,
+               var imagen: Int): Serializable {
 
     fun getID(): Int {
         return this.id
+    }
+
+    fun modificarCantidad(modificador: Int) {
+        this.cantidad += modificador
     }
 }
