a522b171b34327d6640ff1e438541f78c82933c9
Orlando Cavieres <frizortake@gmail.com>
2020-08-23 02:34:16 -0400

    Creado nuevo activity para acceder a edicion de la ficha de un producto. Completado layout para elemento ficha producto.

---
+++ app/src/main/java/com/example/myapplication/FichaProductoCatalogo.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FichaProductoCatalogo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ficha_producto_catalogo)
+    }
+}
