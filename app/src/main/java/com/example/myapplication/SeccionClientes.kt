ad1b15766e80b6f374a70a6b03b2274ce78350b3
Orlando Cavieres <frizortake@gmail.com>
2020-08-29 03:19:33 -0400

    Documentacion completada para SeccionClientes y SeccionNuevaVenta.

--- app/src/main/java/com/example/myapplication/SeccionClientes.kt
+++ app/src/main/java/com/example/myapplication/SeccionClientes.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import kotlinx.android.synthetic.main.activity_clientes.*
 
+/**
+ * Clase que representa la sección de clientes en la aplicación.
+ */
 class SeccionClientes : AppCompatActivity() {
 
+    // Lista de clientes asociada a la sección
     private var listaClientes: MutableList<Cliente> = mutableListOf()
 
+    // Declaración de elementos y adaptadores de la sección Clintes.
     private lateinit var adaptadorPlanilla: AdaptadorCliente
     private lateinit var volverAlMenu: Button
 
@@ -20,6 +24,7 @@ class SeccionClientes : AppCompatActivity() {
     private lateinit var cuadroBusqueda: EditText
     private lateinit var botonBuscar: ImageButton
 
+    // Credenciales de inicio del usuario
     private var tipoIngreso: Int = 0
     private var nombreUsuario: String = ""
     private var idUsuario = 0
@@ -29,19 +34,25 @@ class SeccionClientes : AppCompatActivity() {
         this.mainSeccionClientes()
     }
 
+    // Función principal que corre el catalogo.
     private fun mainSeccionClientes() {
         setContentView(R.layout.activity_clientes)
 
+        // Actualización inicial de la lista de clientes con la base de datos y posterior orden alfabético.
         this.obtenerListaClientesBaseDatos()
         this.listaClientes.sortBy { it.obtenerNombre() }
 
+        // Obtener credenciales del usuario
         nombreUsuario = intent.getStringExtra("nombreUsuarioLogin").toString()
         tipoIngreso = intent.getIntExtra("tipoUsuarioLogin", 0)
         idUsuario = intent.getIntExtra("idLogin", 0)
 
+        // Configuración del botón volver al menu.
         volverAlMenu = findViewById(R.id.boton_clientes_volverAlMenu)
         volverAlMenu.setOnClickListener { finish() }
 
+        /* Configuración del botón agregar cliente. Inicia una nueva activity para formulario de
+        nuevo cliente. */
         addCliente = findViewById(R.id.boton_clientes_addCliente)
         addCliente.setOnClickListener {
             val accion = Intent(this, NuevoCliente::class.java)
@@ -50,14 +61,17 @@ class SeccionClientes : AppCompatActivity() {
             startActivity(accion)
         }
 
+        // Inicializar mensaje para planilla Vacia.
         mensajePlanillaVacia = findViewById(R.id.textView_mensajePlanillaVacia)
 
+        // Si lista clientes está vacía lanzar mensaje informativo.
         if (this.listaClientes.isEmpty()) {
             planillaClientes.visibility = ListView.GONE
             mensajePlanillaVacia.visibility = TextView.VISIBLE
         }
 
-        if (tipoIngreso == 0) {
+        // Si usuario es vendedor entonces ocultar opciones de usuario administrador.
+        if (this.tipoIngreso == 0) {
             val iconAdmin = findViewById<ImageView> (R.id.icon_properties_menu)
             iconAdmin.visibility = ImageView.GONE
             val textAdmin = findViewById<TextView> (R.id.txt_modAdministrador)
@@ -65,21 +79,30 @@ class SeccionClientes : AppCompatActivity() {
             addCliente.visibility = Button.GONE
         }
 
+        // Inicializar adaptador para elementos de lista tipo cliente.
         planillaClientes = findViewById(R.id.listView_planillaClientes)
         adaptadorPlanilla = AdaptadorCliente(this, listaClientes, tipoIngreso)
         planillaClientes.adapter = adaptadorPlanilla
     }
 
+    // Modificación de onResume para actualizar datos.
     override fun onResume() {
         super.onResume()
         this.adaptadorPlanilla.notifyDataSetChanged()
         this.mainSeccionClientes()
     }
 
+    /**
+     * Método que permite buscar en el catalogo un producto a partir de su nombre o fracción del mismo.
+     */
     private fun buscarEnCatalogo() {
 
     }
 
+    /**
+     * Método que permite obtener la lista de clientes almacenada en la base de datos, para su uso en
+     * esta sección.
+     */
     private fun obtenerListaClientesBaseDatos() {
         listaClientes = ClasesBD.bD_Cliente(this)
     }
--- app/src/main/java/com/example/myapplication/SeccionNuevaVenta.kt
+++ app/src/main/java/com/example/myapplication/SeccionNuevaVenta.kt
@@ -12,14 +12,21 @@ import android.widget.*
 import org.w3c.dom.Text
 import java.util.*
 
+/**
+ * Clase que representa la sección Nueva Venta y sus atributos como generar carro de compra y resultados
+ * como añadir al registro de ventas de la bas de datos.
+ */
 class SeccionNuevaVenta : AppCompatActivity() {
 
+    // Listas de datos necesarias para realziar carro de compras.
     private var carroCompras: MutableList<Venta> = mutableListOf()
     private var listaProductos: MutableList<Producto> = mutableListOf()
 
+    // Variables que guardan la fecha de venta y el id del cliente si existe.
     private var fechaActual = Calendar.getInstance().time.toString()
     private var idCliente: Int = -1
 
+    // Declaración de los elementos del layout de la sección.
     private lateinit var volverAlMenu: Button
     private lateinit var continuarVenta: Button
     private lateinit var nuevoProductoCarroCompra: ImageButton
@@ -33,37 +40,46 @@ class SeccionNuevaVenta : AppCompatActivity() {
     private lateinit var mostrarTotalVentaProducto: TextView
     private lateinit var mostrarTotalVentaGlobal: TextView
 
+    // Credenciales del usuario
     private var tipoIngreso: Int = 0
     private var nombreUsuario: String = ""
     private var idUsuario = 0
 
+    // Crear elemento para rellenar y meter al carro.
     var nuevaVentaProducto = Venta()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nueva_venta)
 
-        recuperarListaProductosBD()
+        // Se recupera la lista de productos de la base de datos al inicio.
+        this.recuperarListaProductosBD()
 
+        // Obtener credenciales del usuario
         nombreUsuario = intent.getStringExtra("nombreUsuarioLogin").toString()
         tipoIngreso = intent.getIntExtra("tipoUsuarioLogin", 0)
         idUsuario = intent.getIntExtra("idLogin", 0)
 
+        // COnfiguración de los botones volver al menu y continuar venta.
         volverAlMenu = findViewById(R.id.boton_nuevaVenta_volverAlMenu)
         volverAlMenu.setOnClickListener { finish() }
 
         continuarVenta = findViewById(R.id.boton_nuevaVenta_seguirVenta)
         continuarVenta.setOnClickListener { chequearContinuarVenta() }
 
+        // Inicializar variables de guardado de datos para mostrar en formulario.
         mostrarTotalVentaGlobal = findViewById(R.id.textView_resumenVenta_totalVentaGlobal)
 
         listaCarrito = findViewById(R.id.listView_carroCompras)
         adaptadorCarrito = AdaptadorVenta(this, carroCompras)
         listaCarrito.adapter = adaptadorCarrito
 
+        // Configuración del boton añadir nueva compra a carro compras.
         nuevoProductoCarroCompra = findViewById(R.id.boton_nuevaVenta_addCompra)
         nuevoProductoCarroCompra.setOnClickListener { verificarDatosNumericos(nuevaVentaProducto) }
 
+        // Configuración mediante un Text Change Listener automatizado de los cambios del editText para cantidad
+        // del producto.
         elegirCantidad = findViewById(R.id.editNumber_ventas_cantidad)
         elegirCantidad.addTextChangedListener (object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, inicio: Int, contador: Int, despues: Int) {}
@@ -76,6 +92,8 @@ class SeccionNuevaVenta : AppCompatActivity() {
             }
         })
 
+        // Configuración mediante un Text Change Listener automatizado de los cambios del editText para descuento
+        // del producto.
         elegirDescuento = findViewById(R.id.editNumber_ventas_descuento)
         elegirDescuento.addTextChangedListener (object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, inicio: Int, contador: Int, despues: Int) {}
@@ -88,15 +106,18 @@ class SeccionNuevaVenta : AppCompatActivity() {
             }
         })
 
+        // Inicialización de los elementos que muestran precio y total venta de producto.
         mostrarPrecioProducto = findViewById(R.id.textView_ventas_mostrarPrecioProducto)
         mostrarTotalVentaProducto = findViewById(R.id.textView_ventas_mostrarTotalProducto)
 
+        // Inicialización de lo necesario para un spinner funcional de nombres de productos.
         val nombresProductos: MutableList<String> = mutableListOf()
         listaProductos.forEach { nombresProductos.add(it.nombre) }
         elegirProducto = findViewById(R.id.spinner_nombreProductos)
         val adaptadorSpinner: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.spinner_custom, nombresProductos)
         elegirProducto.adapter = adaptadorSpinner
 
+        // Configuración del spinner en tiempo real.
         elegirProducto.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 nuevaVentaProducto.establecerProducto(listaProductos[position])
@@ -106,6 +127,11 @@ class SeccionNuevaVenta : AppCompatActivity() {
         }
     }
 
+    /**
+     * Método que permite verificar si los datos ingresados en el formulario están permitidos y
+     * acorde a los datos conocidos del producto.
+     * @param venta Analiza los datos ingresados en la venta.
+     */
     @SuppressLint("SetTextI18n")
     private fun verificarDatosNumericos(venta: Venta) {
         if (venta.descuento > 25) {
@@ -130,26 +156,33 @@ class SeccionNuevaVenta : AppCompatActivity() {
         }
     }
 
+    /**
+     * Método que permite calcular el total de la venta.
+     */
     private fun calcularTotalVenta(): Int {
         var suma = 0
         carroCompras.forEach { suma += it.obtenerTotal() }
         return suma
     }
 
+    /**
+     * Método que permite discriminar si el carro esta vacio o no para continuar la venta.
+     */
     private fun chequearContinuarVenta() {
         if (carroCompras.isEmpty()) {
             Toast.makeText(this, "El carro de compras está vacío", Toast.LENGTH_SHORT).show()
         }
         if (carroCompras.isNotEmpty()) {
-            /*val accion = Intent(this, ConfirmarVenta::class.java)
-            accion.putExtra("nombreUsuarioLogin", this.nombreUsuario)
-            accion.putExtra("tipoIngresoLogin", this.tipoIngreso)
-            accion.putExtra("totalVenta", this.calcularTotalVenta())
-            startActivity(accion)*/
             confirmarVenta()
         }
     }
 
+    /**
+     * Método que prosigue la venta cambiando el layout y mostrando el resumen de la venta previa a su
+     * confirmación.
+     * Verifica los datos de cliente ingresado si corresponde con la base de datos.
+     * Actualiza finalmente el stock de cada producto en la venta y registra la venta en base de datos.
+     */
     @SuppressLint("SetTextI18n")
     private fun confirmarVenta() {
         setContentView(R.layout.activity_confirmar_venta)
@@ -212,19 +245,32 @@ class SeccionNuevaVenta : AppCompatActivity() {
         }
     }
 
+    /**
+     * Método que permite recuperar de la base de datos la lista de productos disponible.
+     */
     private fun recuperarListaProductosBD(){
         this.listaProductos = ClasesBD.bD_Productos(this)
     }
 
+    /**
+     * Método booleano que permite discriminar si el cliente se encuentra o no en la base de datos.
+     */
     private fun verificarExistenciaCliente(): Boolean {
         return ClasesBD.bD_VerificarCliente(this.idCliente, this)
     }
 
+    /**
+     * Método que permite actualizar el historial del cliente si se tiene un id válido, añadiendo
+     * una venta con su monto y la fecha.
+     */
     private fun actualizarHistorialCliente() {
-        /* TODO metodo que emplea totalVenta, el id del cliente y la fecha/hora para actualizar
+        /* metodo que emplea totalVenta, el id del cliente y la fecha/hora para actualizar
         *   y añadir una entrada al historial del cliente.*/
     }
 
+    /**
+     * Método que permite actualizar el registro de ventas de la aplicación en la base de datos.
+     */
     private fun actualizarRegistroVentas() {
         ClasesBD.bD_RealizarCompra(this.carroCompras, this.idCliente, this.idUsuario,
             this.fechaActual, this.calcularTotalVenta(), 0, this)
